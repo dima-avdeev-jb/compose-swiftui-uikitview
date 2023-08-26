a4c14edf136006c444995d87e9c305a57aa86862
dima.avdeev <dima.avdeev@jetbrains.com>
2023-08-26 18:40:32 +0400

    MKMapView

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,8 +1,23 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitView
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.MapKit.MKMapView
 import platform.UIKit.UIViewController
 
+@OptIn(ExperimentalForeignApi::class)
 fun ChatViewController(): UIViewController = ComposeUIViewController {
-    ChatApp(displayTextField = false)
+    Column {
+        Text("UIKitView")
+        UIKitView(
+            factory = { MKMapView() },
+            modifier = Modifier.size(300.dp),
+        )
+    }
 }
 
 fun sendMessage(text: String) {
